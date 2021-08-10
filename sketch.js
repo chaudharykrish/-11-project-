4d70a7ce6a8cea02e3226f3d88b492b52d2f6cfc
chaudharykrish <88435655+chaudharykrish@users.noreply.github.com>
2021-08-10 13:49:51 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var path;
+var pathImg;
+
+var runer;
+var runnerImg;
+
+
+
+function preload(){
+  //pre-load images
+
+pathImg = loadImage("path.png");
+
+runnerImg = loadAnimation("runner-1.png","runner-2.png");
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+
+  path=createSprite(200,200);
+  path.addImage("path",pathImg);
+  path.velocityY = 4;
+  path.scale = 1.2;
+
+  runner = createSprite(330,330,50,50);
+  runner.addAnimation("runner",runnerImg);
+  runner.scale = 0.1
+
+  
+
+
+}
+
+function draw() {
+  background(0);
+
+  if(keyDown("Left_arrow")) {
+    runner.velocityX = -1;
+  }
+
+  if(keyDown("Ridht_arrow")) {
+    runner.velocityX = 1;
+  }
+
+  if (path.y < 0){
+    path.y = path.width/2;
+  }
+
+
+
+  drawSprites();
+
+}
